591f32e6da4f1a53d38d3f8d0e0d76ae6ea22ac1
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-06-08 23:39:02 +0300

    app fetch data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { useQuery } from '@apollo/client'
 import NavBar from './components/nav-bar'
 import SearchInput from './components/search-input'
+import { GET_ALL_BOOKS } from './services/getBooks'
+import { Book } from './types/book'
 
 function App() {
+  const { loading, error, data } = useQuery<Book[]>(GET_ALL_BOOKS)
+  console.log('data', data)
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
   return (
     <div className='w-full min-h-screen max-w-screen-lg container mx-auto'>
       <header className='py-4 w-full space-y-8'>

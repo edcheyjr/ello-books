31c2813996edc90cba45c11a3d08ecfd178bb83f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-06-08 23:39:10 +0300

    rename

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Provider from './providers/GlobalProvider.tsx'
+import Providers from './providers'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider>
+    <Providers>
       <App />
-    </Provider>
+    </Providers>
   </React.StrictMode>
 )

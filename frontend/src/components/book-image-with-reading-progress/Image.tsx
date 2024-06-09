bf032b000163ac631f1148e4f222a163047bfaa3
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-06-09 23:41:58 +0300

    image book update

--- frontend/src/components/book-image-with-reading-progress/Image.tsx
+++ frontend/src/components/book-image-with-reading-progress/Image.tsx
@@ -4,26 +4,46 @@ type Props = {
   desc?: string
   progress?: number
   className: string
+  bookSize?: 'sm' | 'default'
 }
 
-const Image = ({ imageUrl, desc, progress = 1, className }: Props) => {
+const Image = ({
+  imageUrl,
+  desc,
+  progress = 1,
+  className,
+  bookSize = 'default',
+}: Props) => {
   return (
-    <div className={`h-auto shadow-xl shadow-primary-dark/30 ${className}`}>
+    <div
+      className={`h-auto rounded-l-xl shadow-xl shadow-primary-dark/30 ${className} border`}
+    >
       <img
         src={imageUrl}
         title={`${Math.floor(progress)}% complete`}
         alt={desc}
-        className='size-full rounded-tl-xl border-r border-primary-dark'
+        className='size-full rounded-l-xl border-r border-primary-dark'
         sizes='(max-width: 600px) 500px, (max-width: 1200px) 1000px, 2000px'
       />
-      <div className='rounded-l-lg w-full bg-primary-dark py-1 pl-1'>
-        <div className='w-full rounded-l-full bg-white'>
-          <div
-            className={`rounded-l-full bg-secondary py-1`}
-            style={{ width: `${progress}%` }}
-          />
+      {bookSize == 'default' ? (
+        <div className='rounded-l-xl w-full bg-primary-dark py-1 pl-1'>
+          <div className='w-full rounded-l-full bg-white'>
+            <div
+              className={`rounded-l-full bg-secondary py-1.5`}
+              style={{ width: `${progress}%` }}
+            />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className='rounded-l-xl w-full bg-primary-dark py-0.5 pl-0.5'>
+          <div className='w-full rounded-l-full bg-white'>
+            <div
+              className={`rounded-l-full bg-secondary py-1`}
+              style={{ width: `${progress}%` }}
+            />
+          </div>
+        </div>
+      )}
     </div>
   )
 }

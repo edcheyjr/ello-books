5c9eccd74328c4d8854381d22e814e6da819bc89
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-06-10 16:43:19 +0300

    preferred favorite over bookmark

--- frontend/src/icons/Favorite.tsx
+++ frontend/src/icons/Favorite.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import type { SVGProps } from 'react'
-
-export function MaterialSymbolsFavorite(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={64}
-      height={64}
-      viewBox='0 0 24 24'
-      {...props}
-    >
-      <path
-        fill='currentColor'
-        d='m12 21l-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812T2.388 10.4T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55t2.475-.55q2.35 0 3.925 1.575T22 8.15q0 1.15-.387 2.25t-1.363 2.412t-2.625 2.963T13.45 19.7z'
-      ></path>
-    </svg>
-  )
+import * as React from 'react'
+import { SVGProps } from 'react'
+interface SVGRProps {
+  title?: string
+  titleId?: string
+  desc?: string
+  descId?: string
 }
-
-export function MaterialSymbolsFavoriteOutline(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width={64}
-      height={64}
-      viewBox='0 0 24 24'
-      {...props}
-    >
-      <path
-        fill='currentColor'
-        d='m12 21l-1.45-1.3q-2.525-2.275-4.175-3.925T3.75 12.812T2.388 10.4T2 8.15Q2 5.8 3.575 4.225T7.5 2.65q1.3 0 2.475.55T12 4.75q.85-1 2.025-1.55t2.475-.55q2.35 0 3.925 1.575T22 8.15q0 1.15-.387 2.25t-1.363 2.412t-2.625 2.963T13.45 19.7zm0-2.7q2.4-2.15 3.95-3.687t2.45-2.675t1.25-2.026T20 8.15q0-1.5-1-2.5t-2.5-1q-1.175 0-2.175.662T12.95 7h-1.9q-.375-1.025-1.375-1.687T7.5 4.65q-1.5 0-2.5 1t-1 2.5q0 .875.35 1.763t1.25 2.025t2.45 2.675T12 18.3m0-6.825'
-      ></path>
-    </svg>
-  )
-}
+const Favorite = ({
+  title,
+  titleId,
+  desc,
+  descId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={24}
+    height={24}
+    viewBox='0 0 24 24'
+    fill='currentColor'
+    stroke='currentColor'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth={2}
+    className='lucide lucide-heart'
+    aria-labelledby={titleId}
+    aria-describedby={descId}
+    {...props}
+  >
+    {desc ? <desc id={descId}>{desc}</desc> : null}
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d='M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z' />
+  </svg>
+)
+export default Favorite
